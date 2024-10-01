367884223b40bf41248b82ca94cc0e482352bd15
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-10-01 07:12:31 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/oauth/OAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
